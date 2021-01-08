40dcd17d1d51338360280343c895d0607b2a71f6
Jose Villaverde <jlvbalsa@gmail.com>
2021-01-08 19:41:12 +0100

    Second commit

---
+++ cart/src/bootstrap.js
@@ -0,0 +1,28 @@
+import faker from 'faker';
+
+const mount = (el) => {
+  let cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+  el.innerHTML = cartText;
+};
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-products'
+// We want to inmediatley render our app into that element
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-cart');
+
+  //Assuming our container doesn't have an element with id dev-products
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
+}
+// Context/Situation  #2
+// We are running this file in development or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-cart' exist
+// WE DO NOT WANT try to immediately render the app
+export { mount };
